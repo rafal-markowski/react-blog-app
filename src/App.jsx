88674d77570202163b67b6f3ffd80152f7ce58fa
rafal <rafal_markowski@onet.pl>
2019-02-13 15:54:41 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <span>Hello</span>
+        );
+    }
+}
+
+export default App;
