65757ad24f4779c54356f6d0e5820163b4b39f86
rafal <rafal_markowski@onet.pl>
2019-04-13 20:09:06 +0200

    Updates

---
+++ precache-manifest.8e163bb66b57c95bbf698366852ecb31.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "51e340dd8e2b4fcb844d",
+    "url": "/react-blog-app/static/js/main.51e340dd.chunk.js"
+  },
+  {
+    "revision": "6bc73645a5ca7595fc53",
+    "url": "/react-blog-app/static/js/1.6bc73645.chunk.js"
+  },
+  {
+    "revision": "2fb614e4f8b858af77f6",
+    "url": "/react-blog-app/static/js/runtime~main.2fb614e4.js"
+  },
+  {
+    "revision": "caec2c52ef48eff0ccc39233401d624c",
+    "url": "/react-blog-app/index.html"
+  }
+];
